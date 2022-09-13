6260233ff9190a16e7dc5a034e0b72f324d1980e
V.Kukh <emdasoftware@gmail.com>
2022-09-13 15:41:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def get_base(backup, pass_code):
+    cmd = 'signalbackup-tools_win.exe signal-2021-11-19-12-38-24.backup 486746075681940264135041361970 --output tmp\\'
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True)
+    p.wait()
+    print("process finished")
+
+
+def get_data(base):
+    pass
+
+
+def get_result(data):
+    pass
+
+
+def main():
+    file_path = "signal-2021-11-19-12-38-24.backup"
+    pass_code = "486746075681940264135041361970"
+    get_base(file_path, pass_code)
+
+
+if __name__ == '__main__':
+    main()
