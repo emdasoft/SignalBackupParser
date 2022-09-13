83f9f4ddf8440cc94b823f50bd34503d1e7dd663
V.Kukh <emdasoftware@gmail.com>
2022-09-13 17:40:49 +0300

    test with base

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import os
+import sqlite3
 import subprocess
 
 
 def get_base(backup, pass_code):
-    cmd = 'signalbackup-tools_win.exe signal-2021-11-19-12-38-24.backup 486746075681940264135041361970 --output tmp\\'
+    path_tmp = "tmp\\"
+    for f in os.listdir(path_tmp):
+        os.remove(os.path.join(path_tmp, f))
+    cmd = 'signalbackup-tools_win.exe ' + backup + ' ' + pass_code + ' --output ' + path_tmp
+    print(cmd)
     PIPE = subprocess.PIPE
     p = subprocess.Popen(cmd, shell=True)
     p.wait()
+
     print("process finished")
+    return "tmp\\database.sqlite"
 
 
 def get_data(base):
-    pass
+    conn = sqlite3.connect(base)
+    c = conn.cursor()
+    c.execute("SELECT * FROM sms")
+
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    # conn.commit()
+    conn.close()
 
 
 def get_result(data):
@@ -20,7 +36,7 @@ def get_result(data):
 def main():
     file_path = "signal-2021-11-19-12-38-24.backup"
     pass_code = "486746075681940264135041361970"
-    get_base(file_path, pass_code)
+    get_data(get_base(file_path, pass_code))
 
 
 if __name__ == '__main__':
