73d6c4834232ec073cc219c364c06769d6d46047
V.Kukh <emdasoftware@gmail.com>
2022-09-19 18:32:39 +0300

    add_get_contacts_and_attachments

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ from tkinter import filedialog, ttk
 import xlsxwriter as xlsxwriter
 import my_icon
 import tkinter.messagebox as mb
+import datetime
+
+
+def get_contacts(row):
+    _id = row[0]
+    recipient_phone = row[1]
+    recipient_system_display_name = row[2]
+    recipient_signal_profile_name = row[3]
+    data = (_id, recipient_system_display_name, recipient_signal_profile_name, recipient_phone)
+    return data
+
+
+def get_attachments(row):
+    _id = row[0]
+    date = datetime.datetime.utcfromtimestamp(int((row[1]) / 1000) + 10800).strftime('%d.%m.%Y %H:%M:%S')
+    date_receive = datetime.datetime.utcfromtimestamp(int((row[2]) / 1000) + 10800).strftime('%d.%m.%Y %H:%M:%S')
+    body = row[3]
+    # address = row[4]
+    quote = row[5]
+    type_f = row[6]
+    size_f = row[8]
+    name_f = row[7]
+    recipient_phone = row[9]
+    recipient_system_display_name = row[10]
+    recipient_signal_profile_name = row[11]
+    data = (_id, date, date_receive, body, recipient_phone, recipient_system_display_name,
+            recipient_signal_profile_name, quote, type_f, size_f, name_f)
+    return data
 
 
 def get_data(row):
@@ -25,13 +53,16 @@ def get_data(row):
              }
     sms_id = row[0]
     address = row[1]
-    date = row[2]
-    date_sent = row[3]
+    date = datetime.datetime.utcfromtimestamp(int((row[2]) / 1000) + 10800).strftime('%d.%m.%Y %H:%M:%S')
+    date_sent = datetime.datetime.utcfromtimestamp(int((row[3]) / 1000) + 10800).strftime('%d.%m.%Y %H:%M:%S')
     if row[4] == 1:
         read_state = 'Прочитанное'
     else:
         read_state = 'Непрочитанное'
-    direction = types[str(row[5])]
+    try:
+        direction = types[str(row[5])]
+    except:
+        direction = str(row[5])
     body = row[6]
     server_guid = row[7]
     recipient_phone = row[8]
@@ -46,7 +77,19 @@ def get_data(row):
 
 def make_result(base):
     workbook = xlsxwriter.Workbook('out/Signal.xlsx')
+    bold = workbook.add_format({'bold': True, 'bg_color': '#c0c0c0', 'align': 'center',
+                                'font_name': 'Arial', 'font_size': '10'})
+    regular = workbook.add_format({'bold': False, 'font_name': 'Arial', 'font_size': '10', 'text_wrap': 1,
+                                   'valign': 'top'})
+
     worksheet_contacts = workbook.add_worksheet('Контакты')
+    worksheet_contacts.set_column('A:D', 40)
+    worksheet_contacts.write('A1', 'UUID', bold)
+    worksheet_contacts.write('B1', 'Имя абонента', bold)
+    worksheet_contacts.write('C1', 'Имя абонента (Signal)', bold)
+    worksheet_contacts.write('D1', 'Номер абонента', bold)
+    worksheet_contacts.autofilter('A1:D1')
+    worksheet_contacts.freeze_panes(1, 0)
 
     worksheet_messages = workbook.add_worksheet('Сообщения')
     worksheet_messages.set_column('A:A', 18)
@@ -56,23 +99,36 @@ def make_result(base):
     worksheet_messages.set_column('I:I', 30)
     worksheet_messages.set_column('J:J', 50)
     worksheet_messages.set_column('K:K', 34)
-    bold = workbook.add_format({'bold': True, 'bg_color': '#c0c0c0', 'align': 'center',
-                                'font_name': 'Arial', 'font_size': '10'})
-    regular = workbook.add_format({'bold': False, 'font_name': 'Arial', 'font_size': '10', 'text_wrap': 1,
-                                   'valign': 'top'})
+
     worksheet_messages.write('A1', 'ID сообщения', bold)
-    worksheet_messages.write('B1', 'ID абнента', bold)
+    worksheet_messages.write('B1', 'ID абонента', bold)
     worksheet_messages.write('C1', 'Имя абонента', bold)
     worksheet_messages.write('D1', 'Имя абонента (Signal)', bold)
     worksheet_messages.write('E1', 'Номер абонента', bold)
-    worksheet_messages.write('F1', 'Время', bold)
-    worksheet_messages.write('G1', 'Время отправки', bold)
+    worksheet_messages.write('F1', 'Время (Минск)', bold)
+    worksheet_messages.write('G1', 'Время отправки (Минск)', bold)
     worksheet_messages.write('H1', 'Статус', bold)
     worksheet_messages.write('I1', 'Тип сообщения', bold)
     worksheet_messages.write('J1', 'Текст', bold)
     worksheet_messages.write('K1', 'GUID', bold)
     worksheet_messages.autofilter('A1:K1')
     worksheet_messages.freeze_panes(1, 0)
+
+    worksheet_att = workbook.add_worksheet('Вложения')
+    worksheet_att.set_column('A:K', 40)
+    worksheet_att.write('A1', 'ID', bold)
+    worksheet_att.write('B1', 'Время (Минск)', bold)
+    worksheet_att.write('C1', 'Время получения (Минск)', bold)
+    worksheet_att.write('D1', 'Текст', bold)
+    worksheet_att.write('E1', 'Номер абонента', bold)
+    worksheet_att.write('F1', 'Имя абонента', bold)
+    worksheet_att.write('G1', 'Имя абонента (Signal)', bold)
+    worksheet_att.write('H1', 'Текст 2', bold)
+    worksheet_att.write('I1', 'Тип вложения', bold)
+    worksheet_att.write('J1', 'Имя файла', bold)
+    worksheet_att.write('K1', 'Размер файла (байт)', bold)
+    worksheet_att.autofilter('A1:K1')
+    worksheet_att.freeze_panes(1, 0)
 
     try:
         conn = sqlite3.connect(base)
@@ -89,9 +145,38 @@ def make_result(base):
                 col += 1
             row_xlsx += 1
         # conn.commit()
+
+        c.execute(
+            "SELECT recipient.uuid, recipient.phone, recipient.system_display_name, recipient.signal_profile_name "
+            "FROM recipient")
+        rows_cont = c.fetchall()
+        row_xlsx = 1
+        for row in rows_cont:
+            col = 0
+            for item in (get_contacts(row)):
+                worksheet_contacts.write(row_xlsx, col, item, regular)
+                col += 1
+            row_xlsx += 1
+
+        c.execute(
+            "SELECT mms._id, mms.date, mms.date_received, mms.body, mms.address, mms.quote_body, part.ct, "
+            "part.data_size, part.file_name, recipient.phone, recipient.system_display_name, "
+            "recipient.signal_profile_name FROM mms INNER JOIN part ON part.mid = mms._id "
+            "INNER JOIN recipient ON recipient._id = mms.address")
+
+        rows_att = c.fetchall()
+        row_xlsx = 1
+        for row in rows_att:
+            col = 0
+            for item in (get_attachments(row)):
+                worksheet_att.write(row_xlsx, col, item, regular)
+                col += 1
+            row_xlsx += 1
+
         conn.close()
         msg = "Анализ резервной копии произведен успешно! Отчет сохранен в каталог 'out'"
         mb.showinfo("Готово!", msg)
+
     except Exception as e:
         msg = "Возникла ошибка по причине: " + str(e) + "\nУбедитесь в корректности введенных данных"
         mb.showerror("Ошибка", msg)
